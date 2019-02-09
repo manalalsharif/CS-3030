7909ee5d3c49cfc0260981b4f40035e47d14dc97
manalalsharif <35620686+manalalsharif@users.noreply.github.com>
2019-02-09 00:55:14 -0700

    just added getopts

--- seds.sh
+++ seds.sh
@@ -6,4 +6,41 @@ sed -i -e 's/\//./g' presidents.csv
 awk -F"," 'NR==27, NR==45 { printf "%-30s %-15s %0s\n", $2, "from " $4, "to " $5  }' presidents.csv
 echo "presidents----"
 
+#step 5
 
+function helpp(){
+          echo "Usage: ./$(basename "$0") -s sedsrc -a awksrc -i inputFile"
+}
+function main(){
+if [[ "$@" == "--help" ]]; then
+	helpp
+else
+	check_arg $@
+fi
+}
+function usage(){
+	echo "Missing arguments"
+	echo "Usage: ./$(basename "$0") -s sedsrc -a awksrc -i inputFile"
+}
+#----------------------------------------
+function check_arg(){
+while getopts s:a:i: opt; do
+case $opt in
+	 s) sedsrc="$OPTARG";;
+	 a) awksrc="$OPTARG";;
+	 i) inputFile="$OPTARG";;
+	\?) echo "Invalid argument"
+	    echo "Usage: ./$(basename "$0") -s sedsrc -a awksrc -i inputFile"
+            exit 1;;
+	    esac
+done
+shift $((OPTIND-1))
+if [[ "$sedsrc" = "" ]]; then
+        usage
+elif [[ "$awksrc" = "" ]]; then
+        usage
+elif [[ "$inputFile" = "" ]]; then
+        usage
+fi      
+}
+main $@
